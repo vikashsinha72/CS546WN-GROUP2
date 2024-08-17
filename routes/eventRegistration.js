1d53b07c981d809b1ba587da3f0b89595a56b928
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2024-08-17 12:21:55 -0400

    Merged from Main

--- routes/eventRegistration.js
+++ routes/eventRegistration.js
@@ -3,44 +3,6 @@ import eventsData from '../data/events.js';
 import eventsRegistrationData from '../data/eventRegistration.js';
 const router = express.Router();
 
-// router.route('/').get((req, res) => {
-//     res.render('homepage', { title: 'Event Management System' });
-// })
-
-// router.route('/createEvent').get((req, res) => {
-//     res.render('createEvent', { title: 'Create Event' });
-// })
-//     .post(async (req, res) => {
-//       const { userName, 
-//         eventName, 
-//         date, 
-//         location, 
-//         category, 
-//         description, 
-//         nearByPort, 
-//         eventMode, 
-//         registrationFee,
-//         contactPerson} = req.body;
-//       try {
-//         const newEvent = await eventsData.createEvent(
-//           userName, 
-//           eventName, 
-//           date, 
-//           location, 
-//           category, 
-//           description, 
-//           nearByPort, 
-//           eventMode, 
-//           registrationFee,
-//           contactPerson
-//          );
-//           res.status(200).json(newEvent);
-//     }
-//     catch (e) {
-//       res.status(400).json({ error: e });
-//     }
-//     })
-
 router.route('/')
   .get(async(req, res) => {
     try{
